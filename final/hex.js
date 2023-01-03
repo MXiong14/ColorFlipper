b92c6908d030b9ef9aed211e661f01e6ae29ac36
Matt Xiong <mattxiong_14@hotmail.com>
2023-01-03 15:09:01 -0600

    added notes for explanation of for loop

--- final/hex.js
+++ final/hex.js
@@ -2,3 +2,22 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+btn.addEventListener('click', function(){
+    // Explaination of for loop 
+    // i = 0 sets a variable before the loop starts. 
+    // i < 6 defines the condition for the loop to run (i must be less than 6). 
+    // i++ increases the value each time the code of block in the loop as been executed.  
+
+    let hexColor = "#";
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[getRandomNumber()];
+    }
+    // console.log(hexColor)
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+};
